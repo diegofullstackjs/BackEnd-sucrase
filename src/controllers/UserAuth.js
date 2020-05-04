ef76cfbe0545ca6606228ffcbd3d24dedebe04d6
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-04 23:56:48 +0100

    Implementando a model e guardando dados no mongo

--- src/controllers/UserAuth.js
+++ src/controllers/UserAuth.js
@@ -1,23 +1,11 @@
-import * as Yup from 'yup'
 
+import UserModel from '../models/User'
 class UserAuth {
 
     async create(req,res){
-        let schema = Yup.object().shape({
-            name: Yup.string().required("Campo nome deve ser preenchido"),
-            email: Yup.string().email().required("Email deve ser Valido"),
-            password: Yup.string().trim()
-            .max(20,"Sua senha ultrapassa 20 caracters")
-            .min(3,"Senha deve ter minimo de 3 caracters")
-            .required("Campo senha é obrigatorio")
+            const create = await UserModel.create(req.body);
 
-        })
-
-         await schema.validate(req.body).then(() => {
-             return res.status(200).json(req.body)
-         }).catch((e) => {
-            return res.status(200).json(e)
-         });
+             return res.status(200).json(create);
         
     }
 }
