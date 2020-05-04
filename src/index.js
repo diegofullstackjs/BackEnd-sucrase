4c679e8089dcc5c69aed05523c601aef7e1e0a19
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-04 23:57:17 +0100

    Resolvendo pequeno bug

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import Mongo from './config/mongoose'
 import IndexRoutes from './routes/index'
 import UsersRoutes from './routes/users'
 import express from 'express'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 class Server {
 
     constructor() {
         this.express = express()
         this.port = process.env.PORT || 3000
-        this.rotas()
         this.middlewares()
+        this.rotas()
         this.mongoose = new Mongo();
     }
 
     middlewares(){
         this.express.use(express.json())
        this.express.use(cors())
+       this.express.use(bodyParser.json())
+       this.express.use(bodyParser.urlencoded({extended:true}))
     }
     rotas(){
         this.express.use('/',IndexRoutes)
