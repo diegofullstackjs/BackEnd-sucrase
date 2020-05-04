ecdfba59a867c53d5ddd94a5f67d8dec0d3c397c
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-04 23:57:56 +0100

    Coloquei middleware de validaçao formulario

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 import {Router} from 'express';
-import UserAuth from '../controllers/UserAuth'
+import UserAuth from '../controllers/UserAuth';
+import {createAuser} from '../validations/UserCreate'
 const routes = new Router();
-routes.post('/create',UserAuth.create)
+routes.post('/create',[createAuser],UserAuth.create)
 
 export default routes;
