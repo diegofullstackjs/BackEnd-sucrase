55723db4ec53af36c3a6911490b93a642fc2d6f3
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-05 13:21:39 +0100

    Adcionando a rota de authentificaçao

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,5 +3,5 @@ import UserAuth from '../controllers/UserAuth';
 import {createAuser} from '../validations/UserCreate'
 const routes = new Router();
 routes.post('/create',[createAuser],UserAuth.create)
-
+routes.post('/auth',UserAuth.auth)
 export default routes;
