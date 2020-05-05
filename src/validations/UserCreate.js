084d57bb85822ce1b16ff8e4627a6fe74cd64fa3
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-05 13:21:12 +0100

    Retornando messages em portugues

--- src/validations/UserCreate.js
+++ src/validations/UserCreate.js
@@ -2,8 +2,10 @@ import * as Yup from 'yup';
 
 const createAuser = async (req,res,next) => {
     const schema = Yup.object().shape({
-        name: Yup.string().required(),
-        password: Yup.string().required().min(5).max(20),
+        name: Yup.string().required('O campo nome é requerido'),
+        password: Yup.string()
+                  .required("O campo senha é requerido")
+                  .min(5,"Minimo de caracters é 5").max(20, "Voce ultrapassou maximo de caracters 20"),
         email: Yup.string().email().required(),
         type: Yup.mixed().oneOf(['Trabalhador','Empresa']).required()
     });
