651e970cb03d9617c8f35b70dc16db998d184a0a
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-05-05 13:20:43 +0100

    Metodo de authentificaçao

--- src/controllers/UserAuth.js
+++ src/controllers/UserAuth.js
@@ -1,4 +1,4 @@
-
+import {sign} from 'jsonwebtoken'
 import UserModel from '../models/User'
 class UserAuth {
 
@@ -7,6 +7,25 @@ class UserAuth {
 
              return res.status(200).json(create);
         
+    }
+    async auth(req,res){
+        const {email,password} = req.body;
+        try{
+        await UserModel.findOne({email:email}).then((data) => {
+            if(data){
+                if(data.password !== password){
+                    return res.status(200).json({error: "Senha invalida"})
+                }
+                const token = sign({data},process.env.KEY,{ expiresIn: '30d'})
+                return res.status(200).json({data, token: token})
+            }
+            return res.status(200).json({error: "Usuario nao encontrado"})
+        })
+    }catch(e) {
+        console.warn(e)
+        return res.status(500).json({ error: "Ocorreu um erro no servidior"})
+    }
+
     }
 }
 
